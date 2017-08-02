8a37c35807de64e7dd9069f63f08018fd4da87c2
David Johnson <davjohns@outlook.com>
2017-08-01 20:59:41 -0700

    Migration to git

---
+++ HelloCS210.java
@@ -0,0 +1,11 @@
+// CS210 Assignment #0 "Hello CS210"
+// David Johnson
+// Prints a hello world string to the console
+
+public class HelloCS210 {
+    public static void main(String[] args) {
+        System.out.println("\"Quotes\"");
+        System.out.println("Slashes \\//");
+        System.out.println("How '\"confounding' \"\\\" is is!");
+    }
+}
